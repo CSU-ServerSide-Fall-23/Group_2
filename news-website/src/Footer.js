a1c05c633523d1420f983535363f79de2b552932
Shreya <142619333+kolaShreya04@users.noreply.github.com>
2023-11-03 22:26:30 -0400

    Update Footer.js with more data
    Signed-off-by: Shreya <142619333+kolaShreya04@users.noreply.github.com>

--- news-website/src/Footer.js
+++ news-website/src/Footer.js
@@ -4,7 +4,42 @@ const Footer = () => {
   return (
     <footer className="bg-dark text-white">
       <div className="container text-center py-3">
-        <p>&copy; {new Date().getFullYear()} Group-2 News-API</p>
+        <div className="row">
+          <div className="col-md-6">
+            <h4>About Us</h4>
+            <p>We are group of students trying to put a news api together.</p>
+          </div>
+          <div className="col-md-3">
+            <h4>Categories</h4>
+            <ul>
+              <li>Politics</li>
+              <li>Technology</li>
+              <li>Entertainment</li>
+              <li>Sports</li>
+            </ul>
+          </div>
+          <div className="col-md-3">
+            <h4>Contact Us</h4>
+            <address>
+              <p>Email one: kola_shreya@students.columbusstate.edu</p>
+              <p>Email two: furlong_logan@students.columbusstate.edu</p>
+              <p>Email three: furlong_logan@students.columbusstate.edu</p>
+              4225 University Ave<br />
+              Columbus, GA 31907<br />
+              Phone: (706) 507-8800
+            </address>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12">
+            <hr />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12">
+            <p>&copy; {new Date().getFullYear()} Group-2 News-API</p>
+          </div>
+        </div>
       </div>
     </footer>
   );
