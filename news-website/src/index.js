8e0147e765016380eaa1a0b592fa8a86e5c66700
Maria Mills <cxwinterx@gmail.com>
2023-11-17 10:43:18 -0500

    Integrate React Router for Dynamic Route Handling

--- news-website/src/index.js
+++ news-website/src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from "./App";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+    }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+        <RouterProvider router={router} />
   </React.StrictMode>
 );
 
