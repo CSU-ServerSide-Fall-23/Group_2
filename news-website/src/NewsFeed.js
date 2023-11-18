82ec4d18e6ffce6156f8b2937872da56e0e90d13
Shreya <142619333+kolaShreya04@users.noreply.github.com>
2023-11-18 15:09:16 -0500

    Update NewsFeed.js
    Signed-off-by: Shreya <142619333+kolaShreya04@users.noreply.github.com>

--- news-website/src/NewsFeed.js
+++ news-website/src/NewsFeed.js
@@ -3,7 +3,7 @@ import NewsCard from './NewsCard';
 
 const NewsFeed = ({ articles }) => {
   return (
-    <div className="d-flex flex-column flex-grow-1 gap-4 gap-xl-3 my-4 px-md-3 mx-auto mx-xl-5" style={{ maxWidth: '800px', margin: 'auto' }}>
+    <div className="d-flex flex-column flex-grow-1 gap-4 gap-xl-3 my-4 px-md-3 mx-auto mx-xl-5" style={{ maxWidth: '800px', maxheight: '10px',margin: 'auto' }}>
       <h1 className="text-md-start text-white fw-bolder text-uppercase text-sm-center m-0">News Feed</h1>
       {displayedArticles.map((article) => (
         <NewsCard key={article.url} article={article} />
