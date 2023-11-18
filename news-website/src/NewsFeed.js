ac7617e66162c09339d58383bfa779ae7725d7b6
Shreya <142619333+kolaShreya04@users.noreply.github.com>
2023-11-18 12:29:49 -0500

    Update NewsFeed.js
    Signed-off-by: Shreya <142619333+kolaShreya04@users.noreply.github.com>

--- news-website/src/NewsFeed.js
+++ news-website/src/NewsFeed.js
@@ -2,10 +2,12 @@ import React from 'react';
 import NewsCard from './NewsCard';
 
 const NewsFeed = ({ articles }) => {
+  const displayedArticles = articles.slice(0, 5); // Adjust the number as needed
+
   return (
     <div className="d-flex flex-column flex-grow-1 gap-4 gap-xl-3 my-4 px-md-3 mx-auto mx-xl-5">
       <h1 className="text-md-start text-white fw-bolder text-uppercase text-sm-center m-0">News Feed</h1>
-      {articles.map((article) => (
+      {displayedArticles.map((article) => (
         <NewsCard key={article.url} article={article} />
       ))}
     </div>
