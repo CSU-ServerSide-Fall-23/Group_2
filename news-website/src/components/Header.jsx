23999889c9d95fdc230225a43f70bffeced52965
Maria Mills <cxwinterx@gmail.com>
2023-11-29 17:58:41 -0500

    Integrate Cypress for E2E testing and add initial tests
    - Install Cypress
    - Add Cypress 'open' and 'run' scripts to package.json..
    - Create 'index.cy.js' to implement tests for the home page (index).
    - Develop a set of initial tests to validate key functionalities of the home page.
    - Add '.search' class to the search form to facilitate Cypress testing.

--- news-website/src/components/Header.jsx
+++ news-website/src/components/Header.jsx
@@ -19,7 +19,7 @@ function Header() {
         <header>
             <div className="container-fluid d-flex justify-content-between align-items-center p-2 flex-column flex-md-row">
                 <a className="navbar-brand fw-bolder fs-3 text-uppercase" href="#">The Know</a>
-                <form className="d-flex" role="search" onSubmit={handleSearchSubmit}>
+                <form className="d-flex search" role="search" onSubmit={handleSearchSubmit}>
                     <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" value={search} onChange={handleSearch}/>
                     <button className="btn btn btn-danger" type="submit">Search</button>
                 </form>
