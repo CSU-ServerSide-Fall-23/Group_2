68e0a606a91f2c865bd4d9b857571b7d1739d1ff
Maria Mills <cxwinterx@gmail.com>
2023-12-02 00:59:13 -0500

    Refactor Cypress tests with beforeEach

--- news-website/src/NewsFeed.js
+++ news-website/src/NewsFeed.js
@@ -6,8 +6,8 @@ const NewsFeed = ({ articles }) => {
     <div className="row gap-4 gap-xl-3 my-4 px-md-3 mx-auto justify-content-center" >
       <h1 className="text-md-start text-white fw-bolder text-uppercase text-sm-center m-0">News Feed</h1>
         {articles.map((article) => (
-        <div className="col-md-4" col-sm-6 col-xs-12>
-            <NewsCard key={article.url} article={article} />
+        <div key={article.url} className="col-lg-3 col-sm-6 col-xs-12">
+            <NewsCard article={article} />
         </div>
         ))}
     </div>
