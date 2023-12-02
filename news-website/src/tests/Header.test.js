3c2c0092dce313423cf9b1050fbb76d870d06a90
Maria Mills <cxwinterx@gmail.com>
2023-12-02 02:20:09 -0500

    Add more Header unit test and mock useNavigate
    - Add mockNavigate function to simulate navigation in test

--- news-website/src/tests/Header.test.js
+++ news-website/src/tests/Header.test.js
@@ -1,5 +1,6 @@
-import { render, screen } from '@testing-library/react';
+import {fireEvent, render, screen} from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import Header from '../components/Header';
 
 // Needs to be wrapped in Router because of useNavigate
@@ -19,4 +20,29 @@ test('renders search button', () => {
   renderWithRouter(<Header />);
   const linkElement = screen.getByText(/Search/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+// Mock useNavigate
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: jest.fn(),
+}));
+
+describe('Header Component', () => {
+  it('navigates to search page on form submission', () => {
+    const mockNavigate = jest.fn();
+    useNavigate.mockImplementation(() => mockNavigate);
+
+    const { getByPlaceholderText, getByRole } = render(<Header />);
+
+    // user typing
+    const searchInput = getByPlaceholderText('Search');
+    fireEvent.change(searchInput, { target: { value: 'test' } });
+
+    const form = getByRole('search');
+    fireEvent.submit(form);
+
+    // expect navigation to search page
+    expect(mockNavigate).toHaveBeenCalledWith('/search?q=test');
+  });
 });
