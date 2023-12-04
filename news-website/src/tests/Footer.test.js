ac0d39455f217a01701fd4a85df2c39b38a9d894
Shreya <142619333+kolaShreya04@users.noreply.github.com>
2023-12-04 14:46:32 -0500

    Update Footer.test.js
    Signed-off-by: Shreya <142619333+kolaShreya04@users.noreply.github.com>

--- news-website/src/tests/Footer.test.js
+++ news-website/src/tests/Footer.test.js
@@ -1,27 +1,19 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import { useParams } from 'react-router-dom';
 import Footer from '../components/Footer';
 
-test('renders Footer component', () => {
-  render(<Footer />);
+// Mock useParams
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useParams: jest.fn(),
+}));
 
-  // Test About Us section
-  expect(screen.getByText('About Us')).toBeInTheDocument();
-  expect(screen.getByText('We are group of students trying to put a news api together.')).toBeInTheDocument();
+test('renders footer component', () => {
+    useParams.mockReturnValue({ footer: 'politics' });
 
-  // Test Categories section
-  expect(screen.getByText('Categories')).toBeInTheDocument();
-  ['Politics', 'Technology', 'Entertainment', 'Sports'].forEach(category => {
-    expect(screen.getByText(category)).toBeInTheDocument();
-  });
+    const { getByText } = render(<Footer />);
 
-  // Test Contact Us section
-  expect(screen.getByText('Contact Us')).toBeInTheDocument();
-  expect(screen.getByText('Email one: kola_shreya@students.columbusstate.edu')).toBeInTheDocument();
-  expect(screen.getByText('4225 University Ave')).toBeInTheDocument();
-  expect(screen.getByText('Phone: (706) 507-8800')).toBeInTheDocument();
-
-  // Test Copyright section
-  const currentYear = new Date().getFullYear();
-  expect(screen.getByText(`© ${currentYear} Group-2 News-API`)).toBeInTheDocument();
+    const element = getByText(/politics news/i);
+    expect(element).toBeInTheDocument();
 });
