639b9acaf35da2a04522295fbc7fecde568b2e5d
Maria Mills <cxwinterx@gmail.com>
2023-12-04 16:25:20 -0500

    Add direct redirection to the news articles and configure Jest to fix Axios testing issue
    - Update package.json to include a Jest configuration that resolves the Axios import issue during testing.
    - Remove Article components, opting to redirect directly to the news source.
    - Wrap NewsCard components in <a> tags to enable direct redirection to the news article source.
    - Apply <a> tag wrapping to HappeningNow news cards to enable direct redirection to the news article source.

--- news-website/src/NewsCard.js
+++ news-website/src/NewsCard.js
@@ -4,11 +4,13 @@ import placeholder from './assets/images/news-placeholder.jpg';
 const NewsCard = ({ article }) => {
   return (
     <div className="card text-bg-dark" key={article.url}>
-      <img src={article.image || placeholder} className="card-img" alt={article.title || "Untitled"} />
-      <div className="card-img-overlay d-flex align-items-end justify-content-start gap-3">
-        <p className="card-title m-0">{article.source.name ? `${article.source.name.substring(0, 10)}...` : 'Untitled'}</p>
-        <p className="card-text">{article.publishedAt ? article.publishedAt.substring(0, 10) : ''}</p>
-      </div>
+        <a href={article.url} target="_blank" rel="noreferrer" className="text-decoration-none text-reset">
+          <img src={article.image || placeholder} className="card-img" alt={article.title || "Untitled"} />
+          <div className="card-img-overlay d-flex align-items-end justify-content-start gap-3">
+            <p className="card-title m-0">{article.source.name ? `${article.source.name.substring(0, 10)}...` : 'Untitled'}</p>
+            <p className="card-text">{article.publishedAt ? article.publishedAt.substring(0, 10) : ''}</p>
+          </div>
+        </a>
     </div>
   );
 };
--- news-website/src/components/FeaturedNews.jsx
+++ news-website/src/components/FeaturedNews.jsx
@@ -67,11 +67,13 @@ function FeaturedNews() {
                                 <h1 className="text-md-start text-white fw-bolder text-uppercase text-sm-center m-0">Happening Now</h1>
                                 {newsArticles.slice(0, 2).map((article) => (
                                   <div className="card text-bg-dark" key={article.url}>
+                                      <a href={article.url} target="_blank" rel="noreferrer" className="text-decoration-none text-reset">
                                         <img src={article.image || placeholder} className="card-img" alt={article.title || "Untitled"} />
                                         <div className="card-img-overlay d-flex align-items-end justify-content-start gap-3">
                                             <p className="card-title m-0">{article.source.name ? `${article.source.name.substring(0, 10)}...` : 'Untitled'}</p>
                                             <p className="card-text">{article.publishedAt ? article.publishedAt.substring(0, 10) : ''}</p>
                                         </div>
+                                      </a>
                                     </div>
                                 ))}
                             </div>
