872b8b0a8218809e7e46ad91b2b7a0c2822d9b8a
Shreya <142619333+kolaShreya04@users.noreply.github.com>
2023-12-04 14:19:07 -0500

    Update Article.js
    Signed-off-by: Shreya <142619333+kolaShreya04@users.noreply.github.com>

--- news-website/src/Article.js
+++ news-website/src/Article.js
@@ -1,17 +1,23 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 const Article = ({ article }) => {
+  const history = useHistory();
+
+  const redirectToArticle = () => {
+    // Use React Router to navigate to the article page
+    history.push(`/article/${encodeURIComponent(article.title)}`);
+  };
+
   return (
-    <div className="article">
+    <div className="article" onClick={redirectToArticle}>
       <h2>{article.title}</h2>
       <img src={article.image || placeholder} alt={article.title} />
       <p>{article.description}</p>
       <p>
         Source: {article.source.name}, Published At: {article.publishedAt}
       </p>
-      <a href={article.url} target="_blank" rel="noopener noreferrer">
-        Read more
-      </a>
+      <p className="redirect-link">Click to read more</p>
     </div>
   );
 };
